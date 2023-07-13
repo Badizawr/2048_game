b192f15a2a882976143e0a650590dbe8940a56b7
Badizawr <vicodin-13@ya.ru>
2023-07-13 17:37:18 +0500

    finish first leson

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello, world")
+def prety_print(mas):
+    print('-' * 10)
+    for row in mas:
+        print(*row)
+    print('-' * 10)
+
+
+def get_empty_list(mas):
+    empty = []
+    for i in range(4):
+        for j in range(4):
+            if mas[i][j] == 0:
+                num = get_number_from_index(i, j)
+                empty.append(num)
+    return empty
+
+
+def get_number_from_index(i,j):
+    return i * 4 + j + 1
+
+mas = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+
+mas[1][2] = 4
+mas[3][0] = 4
+
+print(get_empty_list(mas))
+prety_print(mas)
