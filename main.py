c8ee0550a2ad0a55c7a666faa3f54afe2a454680
Badizawr <bravis13@gmail.com>
2023-07-17 23:38:28 +0500

    completed the graphical interface

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pygame
 import sys
 from logics import *
     
-
+COLORS = {
+    0: (130, 130, 130),
+    2: (225, 230, 225),
+    4: (225, 230,112),
+    8: (225, 230, 0),
+    
+}
 WHITE = (255, 255, 255)
 GRAY = (130, 130, 130)
+BLACK = (0, 0, 0)
 
 BLOCKS = 4
 SIZE_BLOCK = 110
@@ -12,6 +19,25 @@ MARGIN = 10
 WIDTH = BLOCKS * SIZE_BLOCK + (BLOCKS + 1) * MARGIN
 HEIGTH = WIDTH + 110
 TITLE_REC = pygame.Rect(0, 0, WIDTH,110)
+
+
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_REC)
+    font = pygame.font.SysFont("stxingkai", 70)
+    prety_print(mas)
+    for row in range(BLOCKS):
+        for column in range(BLOCKS):
+            value = mas[row][column]
+            text = font.render(f'{value}', True, BLACK)
+            w = column * SIZE_BLOCK + (column + 1) * MARGIN
+            h = row * SIZE_BLOCK + (row + 1) * MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w, h, SIZE_BLOCK, SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK - font_w) / 2
+                text_y = h + (SIZE_BLOCK - font_h) / 2
+                screen.blit(text, (text_x, text_y))
+
 
 mas = [
     [0, 0, 0, 0],
@@ -29,6 +55,8 @@ prety_print(mas)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption("2048")
+draw_interface()
+pygame.display.update()
 
 while is_zero_in_mas(mas):
     for event in pygame.event.get():
@@ -36,20 +64,11 @@ while is_zero_in_mas(mas):
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.KEYDOWN:
-            pygame.draw.rect(screen, WHITE, TITLE_REC)
-            for row in range(BLOCKS):
-                for column in range(BLOCKS):
-                    w = column * SIZE_BLOCK + (column + 1) * MARGIN
-                    h = row * SIZE_BLOCK + (row + 1) * MARGIN + 110
-                    pygame.draw.rect(screen, GRAY, (w, h, 110, 110))
-                    
-            #input()
             empty = get_empty_list(mas)
             random.shuffle(empty)
             random_num = empty.pop()
             x, y = get_index_from_number(random_num)
             mas = insert_2_or_4(mas, x, y)
             print(f'Мы заполнили элемент под номером {random_num}')
-            prety_print(mas)
-    pygame.display.update()
-            
+            draw_interface()
+            pygame.display.update()
